885baae9e2fe1175e582882cf35844000afd7e83
carloslcalleros <70032862+carloslcalleros@users.noreply.github.com>
2020-10-13 20:58:27 -0700

    Update scrip.py

--- scrip.py
+++ scrip.py
@@ -1,7 +1,8 @@
 from Bio.Seq import Seq
-from Bio.SeqFeautures import SeqFeatures, FeaturesLocation
+from Bio.SeqFeauture import SeqFeature,FeatureLocation
 from Bio import SeqIO 
 from Bio.SeqRecord import SeqRecord
 def summarize_contents(filename):
-        prueba = SeqIO.read("filename","genbank")
-        print(prueba)
+        prueba = SeqIO.read(filename,"genbank")
+        print(record)
+summarize_contents(filename)
