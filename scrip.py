c4e60368a8637ae130ce8cd0c11cecbd67291227
carloslcalleros <70032862+carloslcalleros@users.noreply.github.com>
2020-10-14 11:16:49 -0700

    Update scrip.py

--- scrip.py
+++ scrip.py
@@ -1,8 +1,14 @@
-from Bio.Seq import Seq
-from Bio.SeqFeauture import SeqFeature,FeatureLocation
-from Bio import SeqIO 
+from Bio import SeqIO
+from Bio.SeqFeature import FeatureLocation, SeqFeature
 from Bio.SeqRecord import SeqRecord
 def summarize_contents(filename):
-        prueba = SeqIO.read(filename,"genbank")
-        print(record)
+        record = SeqIO.read(filename, "genbank")
+        print("Name: ", record.name)
+        import os
+        print ("Path: ", os.path.dirname(filename))
+        records = list(SeqIO.parse(filename, "genbank"))
+        print("num_records = %i records" % len(records))
+        for seq_record in SeqIO.parse(filename,"genbank"):
+                print("ID:",record.id)
+                ##Aqui debe ir lo de location
 summarize_contents(filename)
