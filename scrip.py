c781e67f0de857e04701da5ed487283ec38c1178
carloslcalleros <70032862+carloslcalleros@users.noreply.github.com>
2020-10-14 22:25:01 -0700

    Mejoras hechas en asesorías

--- scrip.py
+++ scrip.py
@@ -1,24 +1,25 @@
-from Bio.Seq import Seq
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 import os
 
 #filename es la direccion de la lista de archivos que la funcion tomará para completar su función
-filename= "/mnt/c/Users/carlo/Desktop/BioInf/ls_orchid.gbk"
+filename = "/mnt/c/Users/carlo/Desktop/BioInf/ls_orchid.gbk"
+lista = []
 
 def summarize_contents(filename):
-        all_records=[]
+        lista = os.path.split(filename)
+        print("file:", all_records[1])
+        all_records = []
+        record = list(SeqIO.parse(filename,"genbank"))
+        print("path: ", os.path.dirname(filename))
+        print("num_records = %i records" % len(record))
+        print("record:")
         for seq_record in SeqIO.parse(filename, "genbank"):
                 all_records.append(seq_record.name)
-                records = list(SeqIO.parse(filename, "genbank"))
-                print(counter, ".-")
-                print("\n")
-                print("Name: ", seq_record.name)
-                print("Path: ", os.path.dirname(filename)) 
-                print("num_records = %i records" % len(records))
                 print("ID:",seq_record.id)
+                print ("Name: ", seq_record.name)
                 print("Description: ", seq_record.description)
-                
+                print("\n\n")
 summarize_contents(filename)
 
 
