e887a459cca83cbad20b3ba91094278483d85d23
carloslcalleros <carloslcalleros@gmail.com>
2020-10-21 20:42:47 -0700

    Corrección

--- scrip.py
+++ scrip.py
@@ -1,26 +1,30 @@
-import Bio
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 import os
 
-#filename es la direccion de la lista de archivos que la funcion tomará para completar su función
-filename = "/mnt/c/Users/carlo/Desktop/ejercicio-biopython/ls_orchid.gbk"
-lista = []
+# Archivo .gbk de mi escritorio, cambiar a la dirección del nuevo archivo .gbk a leer
+filename =  "/mnt/c/Users/carlo/desktop/biopython-notebook/notebooks/data/ls_orchid.gbk"
 
+# Definición de la función summarize_contents
 def summarize_contents(filename):
-        lista = os.path.split(filename)
-        print("file:", all_records[1])
-        all_records = []
-        record = list(SeqIO.parse(filename,"genbank"))
-        print("path: ", os.path.dirname(filename))
-        print("num_records = %i records" % len(record))
-        print("record:")
-        for seq_record in SeqIO.parse(filename, "genbank"):
-                all_records.append(seq_record.name)
-                print("ID:",seq_record.id)
-                print ("Name: ", seq_record.name)
-                print("Description: ", seq_record.description)
-                print("\n\n")
-summarize_contents(filename)
-
-
+	lista = []
+	lista = os.path.split(filename)
+	cadena = " "
+	# File y ruta
+	cadena = ("file: "+ lista[1] + "\npath: " + lista[0])
+	# Número de registros
+	all_records=[]
+	records = list(SeqIO.parse(filename, "genbank"))
+	cadena += ("\nnum_records: " + str(len(records)))
+	cadena += ("\nrecords:")
+	# Registros
+	for seq_record in SeqIO.parse(filename, "genbank"):
+		all_records.append(seq_record.name)
+		cadena += ("\n- id:" + str(seq_record.id))
+		cadena += ("\nname: " + seq_record.name)
+		cadena += ("\ndescription: " + str(seq_record.description))
+		cadena += ("\n")
+	return cadena
+# Llamada a la función summarize_contents
+resultado = summarize_contents(filename)
+print(resultado)
