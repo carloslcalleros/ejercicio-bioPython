044c48a10cae6daa8fdc4ceddd9a84f64665e492
Carlos Calleros <70032862+carloslcalleros@users.noreply.github.com>
2020-10-23 18:00:37 -0700

    Update scrip.py

--- scrip.py
+++ scrip.py
@@ -3,7 +3,7 @@ from Bio.SeqRecord import SeqRecord
 import os
 
 # Archivo de mi escritorio, se debe de cambiar a la dirección del nuevo archivo .gbk a leer
-filename =  "/mnt/c/Users/carlo/desktop/biopython-notebook/notebooks/data/AF323668.gbk"
+filename =  "/mnt/c/Users/carlo/desktop/ejercicio-biopython/data/opuntia.fasta"
 
 def summarize_contents(filename):
 	lista = []
@@ -13,14 +13,14 @@ def summarize_contents(filename):
 	all_records=[]
 	records = list(SeqIO.parse(filename, "genbank"))
 	cadena += ("\nnum_records: " + str(len(records)))
-	cadena += ("\nrecord(s):")
+	cadena += ("\nrecord(s): ")
 	cadena += ("\n----------------------------------------------------------")
 	for seq_record in SeqIO.parse(filename, "genbank"):
 		all_records.append(seq_record.name)
-		cadena += ("\n- id:" + str(seq_record.id))
+		cadena += ("\n- id: " + str(seq_record.id))
 		cadena += ("\nname: " + seq_record.name)
 		cadena += ("\ndescription: " + str(seq_record.description))
-		cadena += ("\n")
 	return cadena
-resultado = summarize_contents(filename)
-print(resultado)
+if __name__=="__main__":
+	resultado = summarize_contents(filename)
+	print(resultado)
