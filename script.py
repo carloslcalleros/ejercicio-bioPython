504f6f70a7ebe35d7d454a1d0072260c6abaf42c
carloslcalleros <carloslcalleros@gmail.com>
2020-11-06 12:36:56 -0700

    Agrega nueva funcion a script.py\n Este commit agrega una función para generar el complemento inverso de la concatenación de dos cadenas de ADN. Para esto se utiliza la librería de biopython.\n[biopython-tarea3-ex1]

--- script.py
+++ script.py
@@ -1,10 +1,16 @@
 from Bio import SeqIO
+from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import os
 
 # NC_002703.gbk solo es un archivo dentro de la carpeta data, se puede cambiar por otros archivos que esten dentro de esta carpeta 
 filename = os.path.abspath("data/NC_002703.gbk") 
 
+#Secuencias ejemplo para probar la función concatenate_and_get_reverse_of_complement
+seq1="ATCGACTC"
+seq2="GTACCATTTTAGGT"
+
+#----------------------------FUNCIÓN 1-----------------------------------
 def summarize_contents(filename):
 	File_List = []
 	File_Extension = []
@@ -19,20 +25,32 @@ def summarize_contents(filename):
 
 	record = list(SeqIO.parse(filename, type_file))
 #Diccionario
-	d = {}
-	d['File:'] = File_List[1]
-	d['Path:'] = File_List[0]
-	d['Num_records:'] = len(record)
+	dictionary = {}
+	dictionary['File:'] = File_List[1]
+	dictionary['Path:'] = File_List[0]
+	dictionary['Num_records:'] = len(record)
 	
-	d['Names:'] = []
-	d['IDs:'] = []
-	d['Descriptions'] = []
+	dictionary['Names:'] = []
+	dictionary['IDs:'] = []
+	dictionary['Descriptions'] = []
 	for seq_rcd in SeqIO.parse(filename,type_file):
-		d['Names:'].append(seq_rcd.name)
-		d['IDs:'].append(seq_rcd.id)
-		d['Descriptions'].append(seq_rcd.description)
-	return d
+		dictionary['Names:'].append(seq_rcd.name)
+		dictionary['IDs:'].append(seq_rcd.id)
+		dictionary['Descriptions'].append(seq_rcd.description)
+	return dictionary
 
 if __name__ == "__main__":
 	resultados = summarize_contents(filename)
 	print(resultados)
+
+
+#----------------------------FUNCIÓN 2-----------------------------------
+
+def concatenate_and_get_reverse_of_complement(secuencia1,secuencia2):
+	concatenate = Seq(secuencia1 + secuencia2)
+	reverse = concatenate.reverse_complement()
+	return reverse
+
+if __name__ == "__main__":
+	resultado = concatenate_and_get_reverse_of_complement(seq1,seq2)
+	print(resultado.upper())
